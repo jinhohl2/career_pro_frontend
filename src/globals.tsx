17359cace0d87ffe48990c69fb35a46136655080
JIN HO LEE <jinholee@JINs-MacBook-Pro.local>
2023-08-20 22:04:12 -0500

    edit url

--- src/globals.tsx
+++ src/globals.tsx
@@ -1,1 +1,1 @@
-export const API_URL = "https://eromrhyte7ehpe5vv3xqhmr5iu0duldb.lambda-url.us-east-1.on.aws/";
+export const API_URL = "https://eromrhyte7ehpe5vv3xqhmr5iu0duldb.lambda-url.us-east-1.on.aws";
