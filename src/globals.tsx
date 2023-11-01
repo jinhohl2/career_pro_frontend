59de87af4c282818d30a5eca718fd6cadec5fcba
JIN HO LEE <0416roylee@gmail.com>
2023-10-31 20:57:53 -0500

    edited api link

--- src/globals.tsx
+++ src/globals.tsx
@@ -1,2 +1,2 @@
-export const API_URL = "https://eromrhyte7ehpe5vv3xqhmr5iu0duldb.lambda-url.us-east-1.on.aws";
+export const API_URL = "https://eromrhyte7ehpe5vv3xqhmr5iu0duldb.lambda-url.us-east-1.on.aws/";
 //export const API_URL = "http://localhost:3000"
